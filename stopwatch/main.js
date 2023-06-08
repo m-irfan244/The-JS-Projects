f5bdc6c01a840ec959063f4d2c0e3faf45aa9e94
m-irfan244 <irfan.baitham123@gmail.com>
2023-06-08 18:58:27 +0500

    INITIAL PROJECT COMMIT

---
+++ stopwatch/main.js
@@ -0,0 +1,46 @@
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const resetButton = document.getElementById("reset");
+
+const minutesLabel = document.getElementById("minutes");
+const secondsLabel = document.getElementById("seconds");
+const millisecondsLabel = document.getElementById("milliseconds");
+
+let intervalId;
+let time = 0;
+
+startButton.addEventListener("click", start);
+stopButton.addEventListener("click", stop);
+resetButton.addEventListener("click", reset);
+
+function start() {
+  clearInterval(intervalId);
+  intervalId = setInterval(() => {
+    time++;
+
+    const milliseconds = pad(time % 100);
+    const seconds = pad(Math.floor(time / 100) % 60);
+    const minutes = pad(Math.floor(time / 6000));
+
+    minutesLabel.textContent = minutes;
+    secondsLabel.textContent = seconds;
+    millisecondsLabel.textContent = milliseconds;
+  }, 10);
+}
+
+function stop() {
+  clearInterval(intervalId);
+}
+
+function reset() {
+  stop();
+  time = 0;
+
+  minutesLabel.textContent = "00";
+  secondsLabel.textContent = "00";
+  millisecondsLabel.textContent = "00";
+}
+
+function pad(number) {
+  return number.toString().padStart(2, "0");
+}
